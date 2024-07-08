3476ec4790eda2a2873093a2421a50f078343172
Mihir Patel <mihirbhalani18@gmail.com>
2024-07-08 15:06:21 +0530

    Add services to this app

---
+++ BLOG-APP/src/appwrite/auth.js
@@ -0,0 +1,56 @@
+import conf from "../confs/conf";
+import { Client, Account } from "appwrite";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password
+      );
+      if (userAccount) {
+        return this.login({ email, login });
+      } else {
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailPasswordSession(email, password);
+    } catch (error) {
+      error;
+    }
+  }
+  async getCurruntUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log("Appwrite service :: getCurruntUser() :: ", error);
+    }
+    return null;
+  }
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      console.log("Appwrite service :: logout() :: ", error);
+    }
+  }
+}
+
+const authService = AuthService();
+
+export default authService;
