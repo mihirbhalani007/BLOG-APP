2f9102b3bc025818f7abce802ac1a5fd0e1e2945
Mihir Patel <mihirbhalani18@gmail.com>
2024-07-19 19:00:53 +0530

    Modify package json

--- BLOG-APP/src/components/PostCard.jsx
+++ BLOG-APP/src/components/PostCard.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function PostCard({ $id, title, featuredImage }) {
   return (
     <Link to={`/post/${$id}`}>
-      <div className="w-full max-w-sm h-80 bg-gray-200 border border-gray-300 rounded-xl p-4 shadow-lg hover:shadow-xl transition-shadow duration-300">
+      <div className="w-full max-w-sm h-80 bg-gray-200 border border-gray-300 rounded-xl p-4 shadow-lg hover:shadow-xl transition-shadow hover:scale-105 duration-300 ">
         <div className="w-full h-48 flex justify-center mb-4 overflow-hidden rounded-xl">
           <img
             src={appwriteService.getFilePreview(featuredImage)}
@@ -19,5 +19,3 @@ function PostCard({ $id, title, featuredImage }) {
 }
 
 export default PostCard;
-
-
